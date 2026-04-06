5ca893e9c72dcda04061fdb2f20d7fe23007634d
Sagar Panwar <sagarpanwar0123@gmail.com>
2026-04-06 16:49:18 +0530

    added new creatives

--- src/data/visitorDailyCreatives.ts
+++ src/data/visitorDailyCreatives.ts
@@ -1,58 +1,64 @@
 export type VisitorCreativeEntry = {
-  date: string;
-  title: string;
-  images: string[];
+	date: string;
+	title: string;
+	images: string[];
 };
 
 export const visitorDailyCreatives: VisitorCreativeEntry[] = [
-  {
-    date: "2026-04-06",
-    title: "Welcome to NBT",
-    images: [
-      "https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0007.jpg",
-    ],
-  },
-  {
-    date: "2026-04-07",
-    title: "Welcome to NBT",
-    images: [
-      "https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0011.jpg",
-      "https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0016.jpg",
-    ],
-  },
-  {
-    date: "2026-04-08",
-    title: "Welcome to NBT",
-    images: [
-      "https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0014.jpg",
-    ],
-  },
-  {
-    date: "2026-04-09",
-    title: "Welcome to NBT",
-    images: [
-      "https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0015.jpg",
-    ],
-  },
-  {
-    date: "2026-04-10",
-    title: "Welcome to NBT",
-    images: [
-      "https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0009.jpg",
-    ],
-  },
-  {
-    date: "2026-04-11",
-    title: "Welcome to NBT",
-    images: [
-      "https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0013.jpg",
-    ],
-  },
-  {
-    date: "2026-04-12",
-    title: "Welcome to NBT",
-    images: [
-      "https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0008.jpg",
-    ],
-  },
+	{
+		date: "2026-04-06",
+		title: "Welcome to NBT",
+		images: [
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0007.jpg",
+		],
+	},
+	{
+		date: "2026-04-07",
+		title: "Welcome to NBT",
+		images: [
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/7.jpeg",
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0011.jpg",
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0016.jpg",
+		],
+	},
+	{
+		date: "2026-04-08",
+		title: "Welcome to NBT",
+		images: [
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/8.jpeg",
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0014.jpg",
+		],
+	},
+	{
+		date: "2026-04-09",
+		title: "Welcome to NBT",
+		images: [
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/9.jpeg",
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0015.jpg",
+		],
+	},
+	{
+		date: "2026-04-10",
+		title: "Welcome to NBT",
+		images: [
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/10.jpeg",
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0009.jpg",
+		],
+	},
+	{
+		date: "2026-04-11",
+		title: "Welcome to NBT",
+		images: [
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/11.jpeg",
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0013.jpg",
+		],
+	},
+	{
+		date: "2026-04-12",
+		title: "Welcome to NBT",
+		images: [
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/12.jpeg",
+			"https://nbt-creatives-images-535002861308-ap-south-1-an.s3.ap-south-1.amazonaws.com/IMG-20260404-WA0008.jpg",
+		],
+	},
 ];
